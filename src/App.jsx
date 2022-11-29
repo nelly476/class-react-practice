b8cbf114bf6f1ab9f4a07d36feeb4e772de4174f
nelly476 <nellybulatova12@gmail.com>
2022-11-29 12:32:33 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>Hr</h1>;
+  }
+}
+
+export default App;
