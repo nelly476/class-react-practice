b8d0e34265bb7446a53db0d49450eb8990ef046c
nelly476 <nellybulatova12@gmail.com>
2022-11-29 19:11:25 +0300

    state in component practice

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,30 @@ import React from "react";
 
 export default class App extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (719) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: false,
+    count: 0,
   };
 
-  toggleFavorite = () => {
-    this.setState((prevState) => ({
-      ...prevState,
-      isFavorite: !prevState.isFavorite,
-    }));
+  add = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  subtract = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
   };
 
   render() {
-    let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png";
-
     return (
-      <main>
-        <article className="card">
-          <img src="./images/user.png" className="card--image" />
-          <div className="card--info">
-            <img
-              src={`../images/${starIcon}`}
-              className="card--favorite"
-              onClick={this.toggleFavorite}
-            />
-            <h2 className="card--name">
-              {this.state.firstName} {this.state.lastName}
-            </h2>
-            <p className="card--contact">{this.state.phone}</p>
-            <p className="card--contact">{this.state.email}</p>
-          </div>
-        </article>
-      </main>
+      <div className="counter">
+        <button className="counter--minus" onClick={this.subtract}>
+          –
+        </button>
+        <div className="counter--count">
+          <h1>{this.state.count}</h1>
+        </div>
+        <button className="counter--plus" onClick={this.add}>
+          +
+        </button>
+      </div>
     );
   }
 }
