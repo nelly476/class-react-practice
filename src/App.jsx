7c0d7e17348dae55d4b3c0613b9e8769ddea6175
nelly476 <nellybulatova12@gmail.com>
2022-11-29 19:04:59 +0300

    add state obj

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,43 @@ import { useState } from "react";
 import "./App.css";
 import React from "react";
 
-class App extends React.Component {
+export default class App extends React.Component {
+  state = {
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  };
+
+  toggleFavorite = () => {
+    this.setState((prevState) => ({
+      ...prevState,
+      isFavorite: !prevState.isFavorite,
+    }));
+  };
+
   render() {
-    return <h1>Hr</h1>;
+    let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png";
+
+    return (
+      <main>
+        <article className="card">
+          <img src="./images/user.png" className="card--image" />
+          <div className="card--info">
+            <img
+              src={`../images/${starIcon}`}
+              className="card--favorite"
+              onClick={this.toggleFavorite}
+            />
+            <h2 className="card--name">
+              {this.state.firstName} {this.state.lastName}
+            </h2>
+            <p className="card--contact">{this.state.phone}</p>
+            <p className="card--contact">{this.state.email}</p>
+          </div>
+        </article>
+      </main>
+    );
   }
 }
-
-export default App;
